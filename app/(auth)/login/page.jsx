83829151df7e1774fd4ad5aca9ee7e9a632be847
Michael Yeates <michael-yeates@outlook.com>
2023-08-20 17:01:29 +0100

    Setup login/signup forms

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,16 @@
+"use client";
+
+import AuthForm from "../AuthForm";
+
 const Login = () => {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("login", email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Log in</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 };
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,9 +1,18 @@
-const SignUp = () => {
+"use client";
+
+import AuthForm from "../AuthForm";
+
+const Signup = () => {
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    console.log("signup", email, password);
+  };
   return (
     <main>
       <h2 className="text-center">Sign up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   );
 };
 
-export default SignUp;
+export default Signup;

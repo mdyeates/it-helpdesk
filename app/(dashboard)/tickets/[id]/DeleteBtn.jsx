b954e247b507aee2e6967fb12e0b56757a3a2ce9
Michael Yeates <michael-yeates@outlook.com>
2023-08-20 22:14:02 +0100

    update

--- app/(dashboard)/tickets/[id]/DeleteBtn.jsx
+++ app/(dashboard)/tickets/[id]/DeleteBtn.jsx
@@ -11,7 +11,7 @@ const DeleteBtn = ({ id }) => {
   const handleClick = async () => {
     setIsLoading(true);
 
-    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+    const res = await fetch(`${location.origin}/api/tickets/${id}`, {
       method: "DELETE",
     });
 
